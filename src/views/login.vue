3ea01c3e09794b70eb0581bdda17ef94918cfa81
hmcx773 <hmcx773@gmail.com>
2019-04-02 23:03:42 +0800

    day01

---
+++ src/views/login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="login">
+    <el-form ref="form" :model="form" class="container">
+      <el-for-item>
+        <div class="avatar">
+          <img src="../assets/avatar.jpg" alt="">
+        </div>
+      </el-for-item>
+      <el-form-item>
+        <el-input v-model="form.usn" prefix-icon="el-icon-search" placeholder="请输入账号"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="form.pwd" show-password prefix-icon="el-icon-search" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+         <el-button type="primary" class="login-btn">主要按钮</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        usn: '',
+        pwd: ''
+      }
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.login {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background-color: #2f4050;
+
+  .container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 400px;
+    padding: 0px 40px 15px 40px;
+    margin: 200px auto;
+    background: white;
+    .avatar {
+      position: relative;
+      left: 50%;
+      width: 120px;
+      height: 120px;
+      margin-left: -60px;
+      margin-top: -60px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      border: 10px solid #fff;
+      box-shadow: 0 1px 5px #ccc;
+      overflow: hidden;
+    }
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
+</style>
